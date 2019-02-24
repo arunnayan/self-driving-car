90316b63d37ee7091a4bb791e4efa43cb460b4db
arun singh <arunnayan.28@gmail.com>
2019-02-23 17:23:39 -0800

    Added Region of intrest : black triangle

--- lanes.py
+++ lanes.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 image = cv2.imread('test_image.jpg')
 
@@ -34,10 +35,28 @@ def canny(image):
     canny = cv2.Canny(blur, 50,150) # 50 and 150 are low and high threshold which are in ration of 1:3 as per documentation
     return canny
 
+# In road their are right lane and left lane which are sperated by dvider
+# we need a traingle which will suggest |R : L| => [: l]   
+def region_of_interest(image):
+    height = image.shape[0] # Y axix -> 0
+    #triangle = np.array([(200,height),(1100,height),(550,250)])
+    polygon = np.array([[(200,height),(1100,height),(550,250)]])
 
+    mask = np.zeros_like(image) # black image of image size
+    # fill the mask with the given triangle/polynomial with white
+    cv2.fillPoly(mask,polygon, 255)
+    return mask
+    
+    
+    
+    
+    
 # creating a copy of an image using numpy 
-# image is mutable variable
+# image is mutable variablexxxxxx
 
 lane_image = np.copy(image)
-cv2.imshow("canny", canny(lane_image))
+canny = canny(lane_image)
+cv2.imshow("canny", region_of_interest(canny))
+#plt.imshow(canny)
+#plt.show()
 cv2.waitKey(0)
